cb9fc84d4df26e797870b24b7aac27bce7c7e47f
wangchenzz <Mac@macdeimac.local>
2016-01-27 17:35:07 +0800

    git can't commit the image ??

--- 1.5-2015/Main/JSTabBarController.m
+++ 1.5-2015/Main/JSTabBarController.m
@@ -40,7 +40,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    
+//     57 104 55
 //     self.tabBar.tintColor = JSColor(255, 59, 70);
 
 
@@ -89,7 +89,7 @@
     //tabbar 主题；
     
     //设置tabbar item 的颜色；
-    self.tabBar.barTintColor = JSColor(255, 59, 70);
+    self.tabBar.barTintColor = JSColor(57, 104, 55);
 
     
     //设置普通状态下的tabbaritem字体；
@@ -138,7 +138,7 @@
     
     
     //设置导航栏颜色；
-    [navi.navigationBar setBarTintColor:JSColor(255, 59, 70)];
+    [navi.navigationBar setBarTintColor:JSColor(57, 104, 55)];
     
     //修改状态栏主题
     [navi.navigationBar setBarStyle:UIBarStyleBlackOpaque];
